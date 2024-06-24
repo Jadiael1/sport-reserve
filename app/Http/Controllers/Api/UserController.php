4e55e8db999b9598080fb45de864cc5f061b4b7f
Jadiael1 <jadiael@hotmail.com.br>
2024-06-24 16:12:01 -0300

    style(UserController): remove unused import

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Exception;
-use Illuminate\Http\Request;
 
 
 class UserController extends Controller
