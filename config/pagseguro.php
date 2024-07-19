94f5a63c42a2fdd45f962fac4f5bff230d9cf948
Jadiael1 <jadiael@hotmail.com.br>
2024-07-19 12:01:11 -0300

    refactor(config): remove default values from env calls

--- config/pagseguro.php
+++ config/pagseguro.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    'email' => env('PAGSEGURO_EMAIL', 'ishraellenrai@gmail.com'),
+    'email' => env('PAGSEGURO_EMAIL'),
     'token' => env('PAGSEGURO_TOKEN'),
-    'tokenSandBox' => env('PAGSEGURO_TOKEN_SANDBOX', '65C90E7EE04A406A8ABB192EFD722293'),
+    'tokenSandBox' => env('PAGSEGURO_TOKEN_SANDBOX'),
     'environment' => env('PAGSEGURO_ENVIRONMENT', 'sandbox'),
-    'appIdSandBox' => env('PAGSEGURO_APP_ID_SANDBOX', 'app5552857845'),
-    'appKeySandBox' => env('PAGSEGURO_APP_KEY_SANDBOX', 'B98F73541111A601148D0F87B7CFFE7F'),
+    'appIdSandBox' => env('PAGSEGURO_APP_ID_SANDBOX'),
+    'appKeySandBox' => env('PAGSEGURO_APP_KEY_SANDBOX'),
     'baseUrl' => env('PAGSEGURO_BASE_URL', 'https://api.pagseguro.com'),
     'baseUrlSandBox' => env('PAGSEGURO_BASE_URL_SANDBOX', 'https://sandbox.api.pagseguro.com'),
 ];
