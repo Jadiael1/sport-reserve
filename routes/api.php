fca06ade04e1bade7a9f1e60de50514452695094
Jadiael1 <jadiael@hotmail.com.br>
2024-07-27 17:34:18 -0300

    refactor(api): reorganize user update route

--- routes/api.php
+++ routes/api.php
@@ -49,8 +49,11 @@ Route::prefix('v1')->group(function () {
             Route::get('/', [UserController::class, 'index'])->name('users.index');
             Route::post('/', [UserController::class, 'store'])->name('users.store');
             Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
-            Route::patch('/{id}', [UserController::class, 'update'])->name('users.update');
+            // Route::patch('/{id}', [UserController::class, 'update'])->name('users.update');
             Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+        });
+        Route::prefix('users')->group(function () {
+            Route::patch('/{id}', [UserController::class, 'update'])->name('users.update');
         });
     });
 
